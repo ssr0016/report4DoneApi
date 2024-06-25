2840f00dd7ed1450fe26cc343302bd20ef26e3bf
ssr0016 <samsonrecluta0016@gmail.com>
2024-06-25 15:52:05 +0800

    first commit

---
+++ repository/report_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+	"reports/model"
+)
+
+type ReportRepository interface {
+	Save(ctx context.Context, report *model.Report) error
+	Update(ctx context.Context, report *model.Report) error
+	Delete(ctx context.Context, reportId int) error
+	FindById(ctx context.Context, reportId int) (*model.Report, error)
+	FindAll(ctx context.Context) ([]model.Report, error)
+	ReportGetById(ctx context.Context, id int) (*model.Report, error)
+}
